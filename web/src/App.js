33d2e9d1a3cac021da453d94253fb63e8d428864
Jacky Lee <dr.jackylee@gmail.com>
2021-05-15 05:51:46 -0700

    Add Material UI

---
+++ web/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const App = () => (
+  <div>
+    <Typography variant="h1">Fast Flux</Typography>
+  </div>
+);
+
+export default App;
