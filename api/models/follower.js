ff9bfee3762285272c19e4a9c2c2a881de4217e2
Nicholas Draper <nickd2225@gmail.com>
2021-05-19 22:26:42 -0400

    created user and follower models

---
+++ api/models/follower.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+const db = require('../database/db-orm')
+
+const Follower = db.define('followers', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    follower: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    following: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    }
+    follow_timestamp: {
+        type: DataTypes.DATE
+    }
+});
--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,39 @@ const db = require('../database/db-orm')
 
 const User = db.define('users',{
     id: {
-        
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    first_name: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING(50)
+    },
+    handle: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        unique: true
+    },
+    profile_picture: {
+        type: DataTypes.BLOB
+    },
+    loc: {
+        type: DataTypes.STRING(50),
+        allowNull: false         
+    },
+    birth_date: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING(80),
+        allowNull: false
     }
-})
+}, {
+    schema: 'ff'
+});
+
+module.exports = User;
